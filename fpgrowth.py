928e11d4d43810b8e2039752f36f1e7e9675032b
vishaljasrotia <vishal.singh8989@gmail.com>
2018-04-06 17:26:59 -0400

    first test comit from eclipse

---
+++ fpgrowth.py
@@ -0,0 +1,39 @@
+"""
+__author__ = "Vishal Jasrotia"
+__credits__ = ["Vishal Jasrotia"]
+__version__ = "1.0.1"
+__maintainer__ = "Vishal Jasrotia"
+__email__ = "jasrotia.vishal@stonybrook.edu
+__status__ = "In Progress"
+"""
+
+import os
+import time
+import logging
+import pandas
+
+from fplogger import FPLogger
+
+
+log = FPLogger()
+print("id : " , id(log))
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+
+
---
+++ fplogger.py
@@ -0,0 +1,27 @@
+"""
+__author__ = "Vishal Jasrotia"
+__credits__ = ["Vishal Jasrotia"]
+__version__ = "1.0.1"
+__maintainer__ = "Vishal Jasrotia"
+__email__ = "jasrotia.vishal@stonybrook.edu
+__status__ = "In Progress"
+"""
+
+
+class FPLogger(object):
+    
+    """ Singleton logger class 
+    """
+    _instance = None
+    
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(FPLogger, cls).__init__(cls, *args, **kwargs)
+        
+        return cls._instance
+            
+            
+    def __init__(self):
+        print("called")
+        
+        
